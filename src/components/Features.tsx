95b91a23f4739829a08319dd88be4aac57c4ebb0
TheAntro <waltherbeach@gmail.com>
2026-05-31 23:59:33 +0300

    Create a simple landing page

---
+++ src/components/Features.tsx
@@ -0,0 +1,34 @@
+import {
+  CalendarHeart,
+  ClipboardClock,
+  MountainSnow,
+  type LucideIcon,
+} from "lucide-react";
+
+function Feature({ icon: Icon, text }: { icon: LucideIcon; text: string }) {
+  return (
+    <div className="py-4 px-8 flex flex-col place-items-center gap-8 lg:gap-12">
+      <Icon aria-hidden="true" className="text-primary w-20 h-20" />
+      <p className="text-semibold text-mist-500">{text}</p>
+    </div>
+  );
+}
+
+export function Features() {
+  return (
+    <div className="mx-auto flex flex-col justify-between h-dvh py-20 lg:py-0 lg:h-auto lg:flex-row gap-6 lg:gap-8">
+      <Feature
+        icon={CalendarHeart}
+        text={"Create aspirational timebound goals for yourself"}
+      />
+      <Feature
+        icon={ClipboardClock}
+        text={"Organize and track daily habits for each goal"}
+      />
+      <Feature
+        icon={MountainSnow}
+        text={"Succeed with determination enabled by clarity"}
+      />
+    </div>
+  );
+}
