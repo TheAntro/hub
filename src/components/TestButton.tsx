8b7cd9afe2c0dabe3a3064bf263fa7a0346e1532
TheAntro <waltherbeach@gmail.com>
2026-05-31 23:59:33 +0300

    Create a simple landing page

--- src/components/TestButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button";
-
-export function TestButton() {
-  return (
-    <Button variant="outline" onClick={() => console.log("Hello world!")}>
-      Click me!
-    </Button>
-  );
-}
